1508378d331c21eb1442d9ecb3e010efaacb349e
ococa <wangchao.personal@gmail.com>
2019-06-15 12:03:03 +0800

    basic 语法

--- examples/greeting.js
+++ examples/greeting.js
@@ -1,14 +1,3 @@
-var User = /** @class */ (function () {
-    function User(fristName, lastName) {
-        this.fristName = fristName;
-        this.lastName = lastName;
-        this.fullName = fristName + ' ' + lastName;
-    }
-    return User;
-}());
-function greeting(person) {
-    return 'hello' + person.fristName + person.lastName;
-}
-;
-var user = new User(fristName, 1, lastName, 'wang');
-console.log(greeting(user));
+var someValue = 'this is a string';
+var strLength = someValue.length;
+var strLength2 = someValue.length;
--- examples/greeting.ts
+++ examples/greeting.ts
@@ -1,25 +1,3 @@
-class User {
-  fullName: string;
-  fristName: string;
-  lastName: string;
-  constructor(fristName: string, lastName: string) {
-    this.fristName = fristName;
-    this.lastName = lastName;
-    this.fullName = fristName + ' ' + lastName;
-  }
-}
-
-
-interface Person {
-  fristName: string;
-  lastName: string;
-}
-
-function greeting(person: Person) {
-  return 'hello' + person.fristName + person.lastName;
-};
-
-let user = new User('test', 'wang')
-
-
-console.log(greeting(user))
+let someValue: any = 'this is a string';
+let strLength: number = (<string>someValue).length;
+let strLength2: number = (someValue as string).length;
